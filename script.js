f341461fb70aae5ac66c6380d96f1c2cc7377ac5
yasmincaetanosilva-gif <yasmin.caetano.silva@escola.pr.gov.br>
2026-06-09 11:03:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-const botao = document.getElementById("btn");
+const hero = document.querySelector(".hero"); 
 
-botao.addEventListener("click", function(){
+function mudarClima(tipo){ 
+    
+    const mensagem = document.getElementById("mensagem-clima");
+    
+    if(tipo === "sol"){ 
+        
+        hero.style.background = 
+        "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1506744038136-46273834b3fb')";
+        
+        hero.style.backgroundSize = "cover"; 
+        hero.style.backgroundPosition = "center"; 
+        
+        mensagem.innerHTML = 
+        "☀️ Clima ensolarado: ótima condição para o crescimento agrícola.";
+    
+    } 
+    
+    else if(tipo === "chuva"){ 
+        
+        hero.style.background = 
+        "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1515694346937-94d85e41e6f0')";
+        
+        hero.style.backgroundSize = "cover"; 
+        hero.style.backgroundPosition = "center";
+        
+        mensagem.innerHTML = 
+        "🌧️ Chuva detectada: economia natural de irrigação."; 
+    
+    } 
+    
+    else if(tipo === "seca"){ 
+        
+        hero.style.background = 
+        "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1472396961693-142e6e269027')"; 
+        
+        hero.style.backgroundSize = "cover";
+        hero.style.backgroundPosition = "center";
 
-    alert("Bem-vindo ao AgroVision🌱");
+        mensagem.innerHTML = 
+        "🔥 Alerta de seca: recomenda-se irrigação sustentável";
 
-});
+    }
+
+}
